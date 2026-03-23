9c2ca98c80b987e82deef75e5fdd9697a3dc27b0
JD Ross <justindylanross@gmail.com>
2026-03-23 11:27:04 -0500

    AI level choice and toggles

--- server/src/aiplayer.js
+++ server/src/aiplayer.js
@@ -1,3 +1,33 @@
+const { AIPlayerV3 } = require('./aiplayer_v3');
 const { AIPlayerLearned } = require('./aiplayer_learned');
 
-module.exports = { AIPlayer: AIPlayerLearned };
+const AI_DIFFICULTY = Object.freeze({
+  EASY: 'easy',
+  HARD: 'hard',
+});
+
+function normalizeAiDifficulty(rawAiDifficulty) {
+  return rawAiDifficulty === AI_DIFFICULTY.EASY ? AI_DIFFICULTY.EASY : AI_DIFFICULTY.HARD;
+}
+
+function createAIPlayer(name, options = null) {
+  const resolvedOptions = options && typeof options === 'object' ? options : {};
+  const aiDifficulty = normalizeAiDifficulty(
+    typeof options === 'string' ? options : resolvedOptions.aiDifficulty,
+  );
+
+  const player = aiDifficulty === AI_DIFFICULTY.EASY
+    ? new AIPlayerV3(name, resolvedOptions.rolloutSamples)
+    : new AIPlayerLearned(name, resolvedOptions.rolloutSamples, resolvedOptions.learnedOptions);
+
+  player.aiDifficulty = aiDifficulty;
+  player.policy_id = aiDifficulty === AI_DIFFICULTY.EASY ? 'v3' : 'learned';
+  return player;
+}
+
+module.exports = {
+  AIPlayer: AIPlayerLearned,
+  AI_DIFFICULTY,
+  normalizeAiDifficulty,
+  createAIPlayer,
+};
